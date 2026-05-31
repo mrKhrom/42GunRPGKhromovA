e6a85ea87112957354c35dda5bf6f11dcac6b7fe
Aleksey Khromov <khromofficial@icloud.com>
2026-05-31 12:07:47 +0300

    new branch

--- Classes/obj/Debug/net10.0/Classes.AssemblyInfo.cs
+++ Classes/obj/Debug/net10.0/Classes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Classes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c9f85653d6f6e660b6a67defd358c3d3a53c45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Classes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Classes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
